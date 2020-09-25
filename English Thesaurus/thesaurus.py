018af07128cebf9fd6bd95c2e8aaba487d96b4be
hasanelmi678 <70804507+hasanelmi678@users.noreply.github.com>
2020-09-25 16:03:17 +0100

    25-09-2020 / Day 1 / 10 mins

--- English Thesaurus/thesaurus.py
+++ English Thesaurus/thesaurus.py
@@ -3,7 +3,11 @@ import json
 data = json.load(open('English Thesaurus/data.json'))
 
 def translate(word):
-    return data[word]
+    word = word.lower()
+    if word in data:
+        return data[word]
+    else:
+        return 'Word not in dictionary'
 
 input_word = input('Enter a word: ')
 
