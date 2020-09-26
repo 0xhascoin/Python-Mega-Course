333020aaad4357eee77b4146224b18c571596a01
hasanelmi678 <70804507+hasanelmi678@users.noreply.github.com>
2020-09-26 12:16:30 +0100

    26-09-2020 / Day 2 / 15 mins

--- English Thesaurus/thesaurus.py
+++ English Thesaurus/thesaurus.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open('English Thesaurus/data.json'))
 
@@ -6,6 +7,13 @@ def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        did_you_mean = input(f'Did you mean {get_close_matches(word, data.keys())[0]}? (Y/N)')
+
+        if did_you_mean.lower() == 'y':
+            # 
+        else:
+            # 
     else:
         return 'Word not in dictionary'
 
