b4a4edb656428267a9057cc4fb9f267c3b57a3f0
hasanelmi678 <70804507+hasanelmi678@users.noreply.github.com>
2020-10-03 14:52:06 +0100

    3-10-2020 / Day 9 / 50 Mins

--- Application 2 Volcano & Population Web Map/Mapping/map1.py
+++ Application 2 Volcano & Population Web Map/Mapping/map1.py
@@ -6,13 +6,34 @@ lat = list(data['LAT'])
 lon = list(data['LON'])
 elev = list(data['ELEV'])
 
-map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
-fg = folium.FeatureGroup(name='My Map')
+def return_color(elev):
+    if elev < 1000:
+        return 'green'
+    elif elev > 999 and elev < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+
+map = folium.Map(location=[38.58, -99.09], zoom_start=6,
+                 tiles="Stamen Terrain", icon='circle')
+
+fgv = folium.FeatureGroup(name='Volcanoes')
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=f'{el} m', icon=folium.Icon(color='red')))
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=f'{el} m',
+                                     fill_color=return_color(el), color='grey', fill_opacity=0.7))
 
-map.add_child(fg)
+fgp = folium.FeatureGroup(name='Population')
+
+fgp.add_child(folium.GeoJson(data=open('./world.json', 'r', encoding='utf-8-sig').read(),
+                            style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000
+                                                      else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 
 map.save('Map1.html')
---
+++ Fixing Programming Errors/errors.py
@@ -0,0 +1,7 @@
+print(1)
+int(9)
+int(999)
+print(2)
+print(333)
+
+a = [1, 2, 3]
