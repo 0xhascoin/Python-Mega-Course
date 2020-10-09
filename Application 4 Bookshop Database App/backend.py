3089a91db2a106e992117d8c6266e97c9e678fa2
hasanelmi678 <70804507+hasanelmi678@users.noreply.github.com>
2020-10-09 14:55:55 +0100

    9-10-2020 / Day 14 / 80 Mins

--- Application 4 Bookshop Database App/backend.py
+++ Application 4 Bookshop Database App/backend.py
@@ -7,12 +7,54 @@ def connect():
     conn.commit()
     conn.close()
 
-connect()
-
-def add(title, author, year, isbn):
+def add(id, title, author, year, isbn):
     conn = psycopg2.connect("dbname='books' user='postgres' password='postgres123' host='localhost' port='5432'")
     cur = conn.cursor()
 
-    cur.execute("INSERT INTO store VALUES (%s, %s, %s)", (item, quantity, price))
+    cur.execute("INSERT INTO book VALUES (%s, %s, %s, %s, %s)", (id, title, author, year, isbn))
     conn.commit()
     conn.close()
+
+def view():
+    conn = psycopg2.connect("dbname='books' user='postgres' password='postgres123' host='localhost' port='5432'")
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM book")
+    rows = cur.fetchall()
+    conn.close()
+
+    return rows
+
+
+def search(title="", author="", year=0, isbn=0):
+    conn = psycopg2.connect("dbname='books' user='postgres' password='postgres123' host='localhost' port='5432'")
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM book WHERE title=%s OR author=%s OR year=%s OR isbn=%s", (title, author, year, isbn))
+    rows = cur.fetchall()
+    conn.close()
+
+    return rows
+
+
+def delete(title):
+    conn = psycopg2.connect("dbname='books' user='postgres' password='postgres123' host='localhost' port='5432'")
+    cur = conn.cursor()
+
+    cur.execute("DELETE FROM book WHERE title =%s", (title,))
+    conn.commit()
+    conn.close()
+
+def update(id, title, author, year, isbn):
+    conn = psycopg2.connect("dbname='books' user='postgres' password='postgres123' host='localhost' port='5432'")
+    cur = conn.cursor()
+
+    cur.execute("UPDATE book SET title =%s, author=%s, year=%s, isbn=%s  WHERE id=%s", (title, author, year, isbn, id))
+    conn.commit()
+    conn.close()
+
+connect()
+# add(3, 'Book two', 'Author two', 2020, 122)
+# delete('Book two')
+update(1, 'New title', 'New author', 1990, 28319073)
+print(view())
